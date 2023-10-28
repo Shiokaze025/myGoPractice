cb02f1f02058bae17cf8beb984237d091baffffb
shiokaze <shiokaze025@gmail.com>
2023-10-29 00:12:51 +0800

    又不是不能跑，明天再重构吧

--- wiki.go
+++ wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -27,14 +28,54 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	// 记录有访问
+	// 记录访问
 	log.Println("viewHandler:", r.URL.Path)
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Printf("%s", err)
+		http.NotFound(w, r)
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("editHandle:", r.URL.Path)
+	title := r.URL.Path[len("/edit/"):]
+	p, errLoadPage := loadPage(title)
+	if errLoadPage != nil {
+		fmt.Printf("errLoadPage: %s", errLoadPage)
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("saveHandle:", r.URL.Path)
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{title, []byte(body)}
+	errSave := p.save()
+	if errSave != nil {
+		fmt.Printf("errSave Error: %s", errSave)
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, err := template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		fmt.Printf("renderTemplate Error: %s", err)
+	} else {
+		errExecute := t.Execute(w, p)
+		if errExecute != nil {
+			fmt.Printf("Execute Error: %s", err)
+		}
+	}
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
