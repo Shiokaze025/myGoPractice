07752427000f70dc957a2d5e056a3948ea46a9f4
shiokaze <shiokaze025@gmail.com>
2023-10-29 22:41:02 +0800

    添加部分重定向,url校验等

--- wiki.go
+++ wiki.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
+// Page 定义页面文件结构体
 type Page struct {
 	Title string
 	Body  []byte
 }
 
+// 加缓存，我也不知道是不是真的缓存
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+// 验证 url 的正则，不会写抄的
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid page url")
+	}
+	return m[2], nil // title 在下标2
+}
+
+// 保存页面文件
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
+// 读取页面文件
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
@@ -27,49 +47,60 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+// 查看页面文件
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// 记录访问
 	log.Println("viewHandler:", r.URL.Path)
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		fmt.Printf("%s", err)
-		http.NotFound(w, r)
+		// 如果页面不存在就重定向到编辑页面
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 	}
 	renderTemplate(w, "view", p)
 }
 
+// 编辑页面文件
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("editHandle:", r.URL.Path)
-	title := r.URL.Path[len("/edit/"):]
-	p, errLoadPage := loadPage(title)
-	if errLoadPage != nil {
-		fmt.Printf("errLoadPage: %s", errLoadPage)
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+	p, err := loadPage(title)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusFound)
 	}
 	renderTemplate(w, "edit", p)
 }
 
+// 保存页面文件
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("saveHandle:", r.URL.Path)
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	body := r.FormValue("body")
 	p := &Page{title, []byte(body)}
 	errSave := p.save()
 	if errSave != nil {
-		fmt.Printf("errSave Error: %s", errSave)
+		// 给客户端返回错误
+		http.Error(w, errSave.Error(), http.StatusFound)
+		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+// html模板读取
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
-		fmt.Printf("renderTemplate Error: %s", err)
-	} else {
-		errExecute := t.Execute(w, p)
-		if errExecute != nil {
-			fmt.Printf("Execute Error: %s", err)
-		}
+		http.Error(w, err.Error(), http.StatusFound)
 	}
 }
 
